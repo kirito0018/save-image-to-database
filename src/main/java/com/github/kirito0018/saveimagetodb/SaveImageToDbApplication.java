7694f0dbfb74ea32391eb56116742a5bc1dec31b
kirito0018 <ewertonbittencourt94@gmail.com>
2024-08-04 20:05:51 -0300

    initial spring boot configuration

---
+++ src/main/java/com/github/kirito0018/saveimagetodb/SaveImageToDbApplication.java
@@ -0,0 +1,13 @@
+package com.github.kirito0018.saveimagetodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveImageToDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaveImageToDbApplication.class, args);
+    }
+
+}
